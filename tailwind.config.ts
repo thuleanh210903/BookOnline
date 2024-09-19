7f9e4579782da01d3c95abfe3041b2144e2be6d2
ThuLe <thuanhle219@gmail.com>
2024-09-19 17:04:16 +0700

    feat: config for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from "tailwindcss";
 
-const colors = require("./themes/colors.ts").default;
-const { fontSize, lineHeight, fontWeight, fontFamilies } =
-  require("./themes/typography.ts").default;
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,25 +9,74 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        ...colors,
+        transparent: "transparent",
+        black: "#000",
+        white: "#fff",
+        bgPrimary: "#FFF6EA",
+        textPrimary: "#3B3030",
+        secondary: "#FFEB55",
+      },
+      spacing: {
+        px: "1px",
+        "0": "0",
+        "1": "0.25rem",
+        "2": "0.5rem",
+        "3": "0.75rem",
+        "4": "1rem",
+        "5": "1.25rem",
+        "6": "1.5rem",
+        "8": "2rem",
+        "10": "2.5rem",
+        "12": "3rem",
+        "13": "3.25rem",
+        "14": "3.5rem",
+        "15": "3.75rem",
+        "16": "4rem",
+        "20": "5rem",
+        "24": "6rem",
+        "32": "8rem",
+        "40": "10rem",
+        "48": "12rem",
+        "56": "14rem",
+        "64": "16rem",
+      },
+
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
       },
 
       fontSize: {
-        ...fontSize,
+        tiny: "8px",
+        extraSmall: "12px",
+        small: "14px",
+        medium: "16px",
+        large: "18px",
+        extraLarge: "20px",
       },
 
-      fontFamily: {
-        ...fontFamilies,
-      },
+      fontFamily: {},
 
       fontWeight: {
-        ...fontWeight,
+        hairline: "100",
+        thin: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+        black: "900",
       },
 
-      lineHeight: {
-        ...lineHeight,
-      }
-
+      lineHeight: {},
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
   plugins: [],
